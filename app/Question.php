fd210b86ef8f5d012c5f4b6e29afe2bcc6094fc5
LiquidJS <tranngocthang.bkdn@gmail.com>
2020-06-13 22:43:32 -0400

    Prepupulate users and questions tables using model factories and seed

--- app/Question.php
+++ app/Question.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Question extends Model
 {   
@@ -12,5 +13,10 @@ class Question extends Model
     ];
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value) {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
     }
 }
