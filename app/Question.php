5bf170a9eeaa5d90fb429167c358e62abf57aa98
LiquidJS <tranngocthang.bkdn@gmail.com>
2020-06-15 08:15:01 -0400

    Design answers table schema and rename answers column to answers_count in questions table

--- app/Question.php
+++ app/Question.php
@@ -29,7 +29,7 @@ class Question extends Model
     }
     
     public  function getStatusAttribute() {
-    	if ($this->answers > 0) {
+    	if ($this->answers_count > 0) {
     		if ($this->best_answer_id) {
     			return 'answered-accepted';
 		    }
@@ -41,4 +41,7 @@ class Question extends Model
 //    public function getBodyHtmlAttribute() {
 //    	return \Parsedown::instace()->text($this->body);
 //    }
+		public function answers() {
+    	return $this->hasMany(Answer::class);
+		}
 }
