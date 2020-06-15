d95babca83cb40fa2dd6606e7411b7f969509e0d
LiquidJS <tranngocthang.bkdn@gmail.com>
2020-06-14 21:52:35 -0400

    Implement authorization using Gates

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -70,7 +70,10 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-      return view('questions.edit', compact('question'));
+    	if (\Gate::denies('update-question', $question)) { // Hoặc allows
+		    Abort(403, 'Access denied');
+	    }
+	    return view('questions.edit', compact('question'));
     }
 
     /**
@@ -82,6 +85,9 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+	    if (\Gate::denies('update-question', $question)) { // Hoặc allows
+		    Abort(403, 'Access denied');
+	    }
       $question->update($request->only('title', 'body'));
       
       return redirect('/questions')->with('success', 'Your question has been updated');
@@ -96,7 +102,10 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        $question->delete();
-        return redirect()->route('questions.index')->with('success', 'You are deleted question successfully!');
+	    if (\Gate::denies('delete-question', $question)) { // Hoặc allows
+		    Abort(403, 'Access denied');
+	    }
+      $question->delete();
+      return redirect()->route('questions.index')->with('success', 'You are deleted question successfully!');
     }
 }
