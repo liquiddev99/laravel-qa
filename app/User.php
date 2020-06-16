7c8bfc94e45e46cd766057bd34f4b65781fc294f
LiquidJS <tranngocthang.bkdn@gmail.com>
2020-06-16 06:17:21 -0400

    Add votes, check, favorite

--- app/User.php
+++ app/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
 	
 		public function answers() {
 			return $this->hasMany(Answer::class);
-		}
+		} 
 		
 		public  function getAvatarAttribute() {
 			$email = $this->email;
